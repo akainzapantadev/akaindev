4136d24eedbe9165adefc95c908721a2da165916
akainzapantadev <akainzapantadev@gmail.com>
2025-03-24 22:46:14 +0800

    Updated cv

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -8,6 +8,6 @@ import mernLogo from "../assets/images/mern.png";
 import arlogo from "../assets/images/arlgrad1.png";
 import sforceLogo from "../assets/images/sflogo.png";
 import reactNat from "../assets/images/reactNat.png";
-import resume from "../assets/others/AKAINZAPANTA.pdf";
+import resume from "../assets/others/zapanta2025.pdf";
 
 export { Footer, Body, resume, githubLogo, arlogo, linkedinLogo, mernLogo, sforceLogo, reactNat };
